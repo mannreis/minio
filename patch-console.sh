90bcb2327954780a0f3376b0912fc87b3db18fb2
Manuel Reis <reis@dkrz.de>
2026-07-01 14:47:05 +0200

    Download modules to ensure cached!

--- patch-console.sh
+++ patch-console.sh
@@ -1,5 +1,7 @@
 breaking_v='v1.7.7-0.20250905210349-2017f33b26e1'
+go mod download "github.com/minio/console@${breaking_v}"
 working_v='v1.7.6'
+go mod download "github.com/minio/console@${working_v}"
 cp -pfr "$(go env GOMODCACHE)/github.com/minio/console@${breaking_v}/" console
 # Copy deleted files/modules from workgin version
 chmod -R +w console
